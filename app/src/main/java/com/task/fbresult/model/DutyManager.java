e19d125d2d4805accc74afe8d5f2d86570e0c4fc
dmitars <dmitriy.tarasienko@gmail.com>
2020-11-21 13:52:08 +0300

    fix: change manager in accordance with new models

--- app/src/main/java/com/task/fbresult/model/DutyManager.java
+++ app/src/main/java/com/task/fbresult/model/DutyManager.java
@@ -1,54 +1,43 @@
 package com.task.fbresult.model;
 
 import android.content.res.Resources;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
 
 import com.task.fbresult.R;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 public class DutyManager {
-    private Duty duty;
-    private Resources resources;
+    private final Duty duty;
+    private final Resources resources;
 
     public DutyManager(Duty duty, Resources resources){
         this.duty = duty;
         this.resources = resources;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public String getDaysLeftAsString(){
-        Date currentDate = getCurrentDate();
-        GregorianCalendar currentCalendar = new GregorianCalendar();
-        currentCalendar.setTime(currentDate);
+        LocalDate dutyDate = duty.getFrom().toLocalDate();
+        LocalDate todayDate = LocalDate.now();
 
-        int daysBetween = (int)(currentDate.getTime()-new Date().getTime())/(24*60*60*1000);
+        long daysBetween = DAYS.between(dutyDate,todayDate);
         if(daysBetween == 0)
             return resources.getString(R.string.today);
         else
             return String.format(resources.getString(R.string.days_left),daysBetween);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public String getDayOfWeek(){
-        Date currentDate = getCurrentDate();
-        GregorianCalendar currentCalendar = new GregorianCalendar();
-        currentCalendar.setTime(currentDate);
-        int dayOfWeek = currentCalendar.get(Calendar.DAY_OF_WEEK);
+        LocalDateTime localDateTime = duty.getFrom();
+        int dayOfWeek = localDateTime.getDayOfWeek().getValue()-1;
         String[]days = resources.getStringArray(R.array.daysOfWeek);
         return days[dayOfWeek];
-    }
-
-    private Date getCurrentDate(){
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.mm.yyyy");
-        Date currentDate = null;
-//        try {
-//            currentDate = sdf.parse(duty.getDate());
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
-        throw new UnsupportedOperationException("method isn't realised yet");
-        //return currentDate;
     }
 }

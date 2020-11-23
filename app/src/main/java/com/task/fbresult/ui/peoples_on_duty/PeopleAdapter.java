3fc11846c3fc51dda933f2ae341d196f4567f180
ViktorHi <fpmi.graskovviktor@gmail.com>
2020-11-23 21:11:25 +0300

    feat: add image support

--- app/src/main/java/com/task/fbresult/ui/peoples_on_duty/PeopleAdapter.java
+++ app/src/main/java/com/task/fbresult/ui/peoples_on_duty/PeopleAdapter.java
@@ -2,17 +2,23 @@ package com.task.fbresult.ui.peoples_on_duty;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Picture;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.constraintlayout.widget.Constraints;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.task.fbresult.R;
@@ -21,7 +27,7 @@ import com.task.fbresult.db.dao.PersonDao;
 import com.task.fbresult.model.PeopleOnDuty;
 import com.task.fbresult.model.Person;
 import com.task.fbresult.ui.adapters.NodeListener;
-import com.task.fbresult.util.DAORequester;
+import com.task.fbresult.util.ScreenUtils;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -35,7 +41,8 @@ public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.ViewHolder
     private final LayoutInflater inflater;
     private final NodeListener listener;
     public final List<Item> items;
-    public final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_TIME;
+    public final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+    public int width;
 
     public PeopleAdapter(Context context, @NonNull List<Item> items, @Nullable NodeListener listener) {
         inflater = LayoutInflater.from(context);
@@ -43,27 +50,38 @@ public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.ViewHolder
         this.listener = listener;
     }
 
+    @SuppressLint("ResourceType")
     @NonNull
     @Override
     public PeopleAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
         View view = inflater.inflate(R.layout.people_on_duty_item, parent, false);
+
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull PeopleAdapter.ViewHolder holder, int position) {
         Item item = items.get(position);
-        if(item.state.contains(PeopleOnDutyState.ME))
+        if (item.state.contains(PeopleOnDutyState.ME))
             holder.markMe();
-        else if(item.state.contains(PeopleOnDutyState.IN_PROGRESS))
+        else if (item.state.contains(PeopleOnDutyState.IN_PROGRESS))
             holder.markInProgress();
-        else if(item.state.contains(PeopleOnDutyState.IN_FUTURE))
+        else if (item.state.contains(PeopleOnDutyState.IN_FUTURE))
             holder.markInFuture();
 
-        Person person = DAORequester.getPersonInPeopleOnDuty(item.people);
-        holder.title.setText(person.getFio());
+        long personId = item.people.getPersonId();
+        Person person = (new PersonDao().get(String.format(PersonDao.GET_USER_WITH_ID, personId))).get(0);
+
+        holder.title.setText(person.getSurname() + " " + person.getName().substring(0, 1) + ". " + person.getPatronymic().substring(0, 1) + ".");
         holder.from.setText(item.people.getFrom().format(formatter));
         holder.to.setText(item.people.getTo().format(formatter));
+        if (item.images != null)
+            holder.image.setImageBitmap(item.images);
+
+        this.width = holder.image.getLayoutParams().width;
+
+
     }
 
     @Override
@@ -75,20 +93,23 @@ public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.ViewHolder
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         public TextView title;
-        public TextView tag;
         public TextView from;
         public TextView to;
         public View view;
+        public ImageView image;
 
+        @SuppressLint("ResourceType")
         public ViewHolder(View view) {
             super(view);
             this.view = view;
 
+            //View byId = view.findViewById(23);
+
             title = view.findViewById(R.id.tvPeopleDutyName);
-            tag = view.findViewById(R.id.tvPeopleDutyPartners);
             from = view.findViewById(R.id.tvPeopleDutyStartTime);
             to = view.findViewById(R.id.tvPeopleDutyEndTime);
             to = view.findViewById(R.id.tvPeopleDutyEndTime);
+            image = view.findViewById(R.id.ivGraphic);
 
             view.setOnClickListener(this);
         }
@@ -99,30 +120,37 @@ public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.ViewHolder
                 listener.nodeClicked(getAbsoluteAdapterPosition());
         }
 
-        public void  markMe(){
+        public void markMe() {
             view.setBackgroundResource(R.drawable.side_lines_red);
         }
 
-        public void  markInProgress(){
+        public void markInProgress() {
             view.setBackgroundResource(R.drawable.side_lines_green);
         }
 
-        public void  markEnded(){
+        public void markEnded() {
             view.setBackgroundResource(R.drawable.side_lines_grey);
         }
 
-        public void  markInFuture(){
+        public void markInFuture() {
             view.setBackgroundResource(R.drawable.side_lines_yellow);
         }
     }
 
-    public static class Item{
+    public static class Item {
         public PeopleOnDuty people;
         public Set<PeopleOnDutyState> state;
+        public Bitmap images;
 
         public Item(PeopleOnDuty people, Set<PeopleOnDutyState> me) {
             this.people = people;
             state = me;
+        }
+
+        public Item(PeopleOnDuty people, Set<PeopleOnDutyState> state, Bitmap images) {
+            this.people = people;
+            this.state = state;
+            this.images = images;
         }
     }
 
@@ -137,8 +165,8 @@ public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.ViewHolder
         @Override
         public void onDrawOver(Canvas c, RecyclerView parent, @NotNull RecyclerView.State state) {
             //divider padding give some padding whatever u want or disable
-            int left =parent.getPaddingLeft()+80;
-            int right = parent.getWidth() - parent.getPaddingRight()-80;
+            int left = parent.getPaddingLeft() + 80;
+            int right = parent.getWidth() - parent.getPaddingRight() - 80;
 
             int childCount = parent.getChildCount();
             for (int i = 0; i < childCount; i++) {

dcf0d398347cf95cc5ce8fa87c6ef73c7e99bda0
ViktorHi <fpmi.graskovviktor@gmail.com>
2020-11-25 01:18:23 +0300

    feat: add time title support

--- app/src/main/java/com/task/fbresult/ui/peoples_on_duty/DutyFragment.java
+++ app/src/main/java/com/task/fbresult/ui/peoples_on_duty/DutyFragment.java
@@ -1,9 +1,12 @@
 package com.task.fbresult.ui.peoples_on_duty;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,15 +18,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.task.fbresult.DutyActivity;
 import com.task.fbresult.R;
 import com.task.fbresult.db.dao.DutyDao;
-import com.task.fbresult.db.dao.DutyTypesDao;
-import com.task.fbresult.db.dao.PeopleOnDutyDao;
 import com.task.fbresult.model.Duty;
 import com.task.fbresult.model.DutyType;
+import com.task.fbresult.model.GraphicResult;
 import com.task.fbresult.model.PeopleOnDuty;
 import com.task.fbresult.ui.adapters.NodeListener;
 import com.task.fbresult.util.DAORequester;
@@ -32,11 +36,17 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-public class DutyFragment extends Fragment implements NodeListener {
+@RequiresApi(api = Build.VERSION_CODES.O)
+public class DutyFragment extends Fragment implements NodeListener{
 
+    private static final int DATA_UPLOADED = 42;
     private PeopleAdapter adapter;
     private Handler leftTimeHandler;
     private TextView leftTime;
@@ -53,7 +63,7 @@ public class DutyFragment extends Fragment implements NodeListener {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_duty, container, false);
-        duty = (Duty)getArguments().getSerializable(DutyActivity.DUTY_PARAMETERS);
+        duty = (Duty) getArguments().getSerializable(DutyActivity.DUTY_PARAMETERS);
 
         LinearLayout dutyHolder = view.findViewById(R.id.duty_frame);
         RecyclerView recycler = view.findViewById(R.id.duty_recycler);
@@ -63,25 +73,37 @@ public class DutyFragment extends Fragment implements NodeListener {
         dutyHolder.addView(dutyInfo, 1);
         setDayText(view, this.duty.getFrom());
 
+        List<PeopleAdapter.Item> orderedListOfPerson = PeoplesProviders.getOrderedListOfPerson(this.duty);
 
-        adapter = new PeopleAdapter(getContext(), PeoplesProviders.getOrderedListOfPerson(this.duty), this);
+
+        adapter = new PeopleAdapter(getContext(),orderedListOfPerson , this);
         recycler.setAdapter(adapter);
         recycler.addItemDecoration(new PeopleAdapter.SimpleDivider(getContext()));
 
         leftTimeHandler = new Handler();
         leftTimeHandler.post(updateTime);
+
+        DutyFragmentViewModel model = new ViewModelProvider(this).get(DutyFragmentViewModel.class);
+        model.getGraphic(duty).observe(getViewLifecycleOwner(), (graphicResult) -> {
+
+            Map<Integer, List<Bitmap>> map = graphicResult.list.stream()
+                    .collect(Collectors.groupingBy(
+                            GraphicResult.Result::getPersonId,
+                            Collectors.mapping(
+                                    GraphicResult.Result::getImage,
+                                    Collectors.toList()
+                            )
+                    ));
+
+            for (PeopleAdapter.Item i : adapter.items)
+                i.images = map.get((int)i.people.getPersonId()).get(0);
+
+            adapter.notifyDataSetChanged();
+        });
+
         return view;
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private Duty getCurrentDuty() {
-        //todo return current duty;
-        return new DutyDao().get(String.format(DutyDao.GET_DUTY_WITH_ID, 1222)).get(0);
-    }
-
-//    List<PeopleOnDuty> getPeoples(Duty duty) {
-//        return new PeopleOnDutyDao().get(String.format(PeopleOnDutyDao.GET_PEOPLE_ON_DUTY_WITH_DUTY_ID, duty.getId()));
-//    }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -89,44 +111,22 @@ public class DutyFragment extends Fragment implements NodeListener {
         Toast.makeText(getContext(), adapter.items.get(indexOf).people.getFrom().toString(), Toast.LENGTH_LONG).show();
     }
 
-    Runnable updateTime = new Runnable() {
-        @SuppressLint("SetTextI18n")
-        @RequiresApi(api = Build.VERSION_CODES.O)
-        @Override
-        public void run() {
-            Duration between = Duration.between(LocalDateTime.now(), duty.getFrom());
-
-            if (between.isNegative()) {
-                if (!Duration.between(LocalDateTime.now(), duty.getTo()).isNegative()) {
-                    leftTime.setText("Remaining time" + getRemainingTime(LocalDateTime.now(), duty.getTo()));
-                    leftTimeHandler.postDelayed(this, 200);
-                } else {
-                    leftTime.setText("Ended");
-                }
-                return;
-            }
-
-            leftTime.setText(getRemainingTime(LocalDateTime.now(), duty.getFrom()));
-            leftTimeHandler.postDelayed(this, 200);
-        }
-    };
-
     @RequiresApi(api = Build.VERSION_CODES.O)
     @SuppressLint("DefaultLocale")
-    private String getRemainingTime(LocalDateTime start, LocalDateTime end){
-        long day = ChronoUnit.DAYS.between  (start,end);
-        long hour = ChronoUnit.HOURS.between(start,end);
+    private String getRemainingTime(LocalDateTime start, LocalDateTime end) {
+        long day = ChronoUnit.DAYS.between(start, end);
+        long hour = ChronoUnit.HOURS.between(start, end);
         long minutes = ChronoUnit.MINUTES.between(start, end);
         long seconds = ChronoUnit.SECONDS.between(start, end);
 
-        if (day ==0){
+        if (day == 0) {
             return String.format("%d:%02d:%02d",
                     hour - day * 24,
                     minutes - hour * 60,
                     seconds - minutes * 60
             );
         }
-        return  String.format("%d %d:%02d:%02d",
+        return String.format("%d %d:%02d:%02d",
                 day,
                 hour - day * 24,
                 minutes - hour * 60,
@@ -160,5 +160,28 @@ public class DutyFragment extends Fragment implements NodeListener {
         ((TextView) view.findViewById(R.id.duty_day)).setText(format);
         return format;
     }
+
+    Runnable updateTime = new Runnable() {
+        @SuppressLint("SetTextI18n")
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        @Override
+        public void run() {
+            Duration between = Duration.between(LocalDateTime.now(), duty.getFrom());
+
+            if (between.isNegative()) {
+                if (!Duration.between(LocalDateTime.now(), duty.getTo()).isNegative()) {
+                    leftTime.setText("Remaining time" + getRemainingTime(LocalDateTime.now(), duty.getTo()));
+                    leftTimeHandler.postDelayed(this, 200);
+                } else {
+                    leftTime.setText("Ended");
+                }
+                return;
+            }
+
+            leftTime.setText(getRemainingTime(LocalDateTime.now(), duty.getFrom()));
+            leftTimeHandler.postDelayed(this, 200);
+        }
+    };
+
 
 }

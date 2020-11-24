0c2408ec44c555c67145ac31daf50f41db5577bd
ViktorHi <fpmi.graskovviktor@gmail.com>
2020-11-25 01:15:56 +0300

    feat: add GraphicResult.java

---
+++ app/src/main/java/com/task/fbresult/model/GraphicResult.java
@@ -0,0 +1,37 @@
+package com.task.fbresult.model;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.io.Serializable;
+import java.util.Base64;
+import java.util.List;
+
+public class GraphicResult implements Serializable {
+    public List<Result> list;
+
+
+    public static class Result implements Serializable {
+        public int personId;
+        public String fileString;
+
+        public int getPersonId() {
+            return personId;
+        }
+
+        public String getFileString() {
+            return fileString;
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        public Bitmap getImage() {
+
+            byte[] decode = Base64.getDecoder().decode(fileString);
+            return BitmapFactory.decodeByteArray(decode, 0, decode.length);
+        }
+    }
+
+}

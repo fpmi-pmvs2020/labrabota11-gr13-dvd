de62380144c40406c44760d25e419aedf417f076
ViktorHi <fpmi.graskovviktor@gmail.com>
2020-12-02 23:16:19 +0300

    feat: add HourlyGraphicResult model

---
+++ app/src/main/java/com/task/fbresult/model/HourlyGraphicResult.java
@@ -0,0 +1,9 @@
+package com.task.fbresult.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class HourlyGraphicResult implements Serializable {
+    public List<List<GraphicResult.Result >> timeMap;
+
+}

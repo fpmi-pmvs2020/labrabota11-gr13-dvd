ed2d0ec1c0992a2ee8d414c1d560617d4d0966f8
dmitars <dmitriy.tarasienko@gmail.com>
2020-12-02 22:43:56 +0300

    feat: add image utils

---
+++ app/src/main/java/com/task/fbresult/util/ImgUtils.java
@@ -0,0 +1,18 @@
+package com.task.fbresult.util;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.ByteArrayOutputStream;
+
+public class ImgUtils {
+    public static byte[] getBitmapAsByteArray(Bitmap bitmap) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, outputStream);
+        return outputStream.toByteArray();
+    }
+
+    public static Bitmap getByteArrayAsBitmap(byte[]bytes){
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+    }
+}

9b846d04b10efd6750b167ee59f18112ff95dd58
dmitars <dmitriy.tarasienko@gmail.com>
2021-04-05 12:51:23 +0300

    feat: remove unnecessary fields and methods from local db

--- app/src/main/java/com/task/fbresult/db/DBFillers.java
+++ app/src/main/java/com/task/fbresult/db/DBFillers.java
@@ -1,16 +1,15 @@
 package com.task.fbresult.db;
 
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.RequiresApi;
 
-import com.task.fbresult.db.dao.DutyDao;
-import com.task.fbresult.db.dao.DutyTypesDao;
-import com.task.fbresult.db.dao.PeopleOnDutyDao;
-import com.task.fbresult.db.dao.PersonDao;
-import com.task.fbresult.db.dao.RoleDao;
+import com.task.fbresult.db.fbdao.FBDutyDao;
+import com.task.fbresult.db.fbdao.FBDutyTypesDao;
+import com.task.fbresult.db.fbdao.FBPeopleOnDutyDao;
+import com.task.fbresult.db.fbdao.FBPersonDao;
+import com.task.fbresult.db.fbdao.FBRoleDao;
 import com.task.fbresult.generators.DutyGenerator;
 import com.task.fbresult.generators.DutyTypesGenerator;
 import com.task.fbresult.generators.PeopleOnDutyGenerator;
@@ -19,35 +18,26 @@ import com.task.fbresult.generators.RoleGenerator;
 
 public class DBFillers {
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public static void cleanTables() {
-        SQLiteDatabase db = DBHelper.getInstance().getWritableDatabase();
-
-        db.delete(DBHelper.DUTY_TABLE, null, null);
-        db.delete(DBHelper.TYPES_TABLE, null, null);
-        db.delete(DBHelper.PERSON_TABLE, null, null);
-        db.delete(DBHelper.ROLE_TABLE, null, null);
-        db.delete(DBHelper.PER_ON_DUTY_TABLE, null, null);
-
-        Log.d(DBHelper.DB_LOG, "information from table was deleted");
-
-    }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public static void fillData() {
+        FBDutyDao dutyDao = new FBDutyDao();
+        FBDutyTypesDao dutyTypesDao = new FBDutyTypesDao();
+        FBPeopleOnDutyDao peopleOnDutyDao = new FBPeopleOnDutyDao();
+        FBPersonDao personDao = new FBPersonDao();
+        FBRoleDao roleDao = new FBRoleDao();
 
-        cleanTables();
-        DutyDao dutyDao = new DutyDao();
-        DutyTypesDao dutyTypesDao = new DutyTypesDao();
-        PeopleOnDutyDao peopleOnDutyDao = new PeopleOnDutyDao();
-        PersonDao personDao = new PersonDao();
-        RoleDao roleDao = new RoleDao();
+        dutyDao.clean();
+        dutyTypesDao.clean();
+        peopleOnDutyDao.clean();
+        personDao.clean();
+        roleDao.clean();
 
         RoleGenerator.generate().forEach(roleDao::save);
         DutyTypesGenerator.generate().forEach(dutyTypesDao::save);
         PersonGenerator.generate().forEach(personDao::save);
         DutyGenerator.generate().forEach(dutyDao::save);
         PeopleOnDutyGenerator.generate().forEach(peopleOnDutyDao::save);
-
+        Log.d("DBFiller","db was filled!");
     }
 }
--- app/src/main/java/com/task/fbresult/db/DBHelper.java
+++ app/src/main/java/com/task/fbresult/db/DBHelper.java
@@ -1,128 +1,46 @@
 package com.task.fbresult.db;
 
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.os.Build;
-import android.util.Log;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-
-import com.task.fbresult.model.Duty;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class DBHelper extends SQLiteOpenHelper {
-
-    private static DBHelper dbHelper;
-
+public class DBHelper{
     //region fields
-    public static final int DB_VERSION = 1;
-    public static final String DB_NAME = "duties_db";
+    public static final String DB_NAME = "fb-result";
     public static final String DB_LOG = "myDbLogs";
     public static final String PER_ON_DUTY_TABLE =              "PeopleOnDuty";
-    public static final String PER_ON_DUTY_ID_COLUMN =          "Id";
-    public static final String PER_ON_DUTY_PERSON_FK_COLUMN =   "PersonId";
-    public static final String PER_ON_DUTY_DUTY_FK_COLUMN =     "DutyId";
-    public static final String PER_ON_DUTY_FROM_COLUMN =        "OnDutyFrom";
-    public static final String PER_ON_DUTY_TO_COLUMN =          "OnDutyTo";
+    public static final String PER_ON_DUTY_ID_COLUMN =          "firebaseId";
+    public static final String PER_ON_DUTY_PERSON_ID_COLUMN =   "personId";
+    public static final String PER_ON_DUTY_DUTY_ID_COLUMN =     "dutyId";
+    public static final String PER_ON_DUTY_FROM_COLUMN =        "from";
+    public static final String PER_ON_DUTY_TO_COLUMN =          "to";
 
     public static final String PERSON_TABLE =                   "People";
-    public static final String PERSON_ID_COLUMN =               "Id";
-    public static final String PERSON_LOGIN_COLUMN =            "Login";
-    public static final String PERSON_FIO_COLUMN =              "FIO";
-    public static final String PERSON_TEL_COLUMN =              "Telephone";
-    public static final String PERSON_ADDRESS_COLUMN =          "Address";
-    public static final String PERSON_BIRTH_COLUMN =            "BirthDate";
-    public static final String PERSON_ROLE_COLUMN =             "RoleId";
-    public static final String PERSON_IMAGE_COLUMN =             "Image";
+    public static final String PERSON_ID_COLUMN =               "firebaseId";
+    public static final String PERSON_LOGIN_COLUMN =            "login";
+    public static final String PERSON_FIO_COLUMN =              "fio";
+    public static final String PERSON_TEL_COLUMN =              "telephone";
+    public static final String PERSON_ADDRESS_COLUMN =          "address";
+    public static final String PERSON_BIRTH_COLUMN =            "birthDate";
+    public static final String PERSON_ROLE_COLUMN =             "roleId";
+    public static final String PERSON_IMAGE_COLUMN =             "image";
 
     public static final String DUTY_TABLE =                     "Duties";
-    public static final String DUTY_ID_COLUMN =                 "Id";
-    public static final String DUTY_FROM_COLUMN =               "DutyFrom";
-    public static final String DUTY_TO_COLUMN =                 "DutyTo";
-    public static final String DUTY_TYPE_FK_COLUMN =            "DutyTypeId";
-    public static final String DUTY_MAX_PEOPLE_COLUMN =         "MaxPeopleOnDuty";
+    public static final String DUTY_ID_COLUMN =                 "firebaseId";
+    public static final String DUTY_FROM_COLUMN =               "from";
+    public static final String DUTY_TO_COLUMN =                 "to";
+    public static final String DUTY_TYPE_ID_COLUMN =            "typeId";
+    public static final String DUTY_MAX_PEOPLE_COLUMN =         "maxPeople";
 
     public static final String ROLE_TABLE =                     "Roles";
-    public static final String ROLE_ID_COLUMN =                 "Id";
-    public static final String ROLE_NAME_COLUMN =               "Name";
+    public static final String ROLE_ID_COLUMN =                 "firebaseId";
+    public static final String ROLE_NAME_COLUMN =               "name";
 
     public static final String TYPES_TABLE =                    "DutyTypes";
-    public static final String TYPES_ID_COLUMN =                "Id";
-    public static final String TYPES_TITLE_COLUMN =             "TypeName";
+    public static final String TYPES_ID_COLUMN =                "firebaseId";
+    public static final String TYPES_TITLE_COLUMN =             "title";
+
+    public static final String MESSAGES_TABLE =                 "messages";
+    public static final String MESSAGES_ID_COLUMN =             "firebaseId";
+    public static final String MESSAGES_AUTHOR_COLUMN =         "authorId";
+    public static final String MESSAGES_RECIPIENT_COLUMN =      "recipientId";
+    public static final String MESSAGES_DUTY_ID_COLUMN =        "dutyId";
+
     //endregion fields
-
-    private DBHelper(@Nullable Context context, @Nullable String name,
-                    @Nullable SQLiteDatabase.CursorFactory factory,
-                    int version) {
-        super(context, name, factory, version);
-    }
-
-
-    public static DBHelper getInstance(@Nullable Context context,
-                                       @Nullable SQLiteDatabase.CursorFactory factory){
-        if(dbHelper==null)
-            dbHelper = new DBHelper(context, DB_NAME, factory, DB_VERSION);
-
-        return dbHelper;
-    }
-
-    public static DBHelper getInstance(){
-
-        if (dbHelper == null){
-            throw new RuntimeException("context don't exist, use another newInstant(_context)");
-        }
-        return dbHelper;
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        String typeTable = "create table " + TYPES_TABLE + "("+
-                TYPES_ID_COLUMN +           " integer primary key autoincrement," +
-                TYPES_TITLE_COLUMN +        " text not null);";
-
-        String roleTable = "create table " + ROLE_TABLE + "("+
-                ROLE_ID_COLUMN +           " integer primary key autoincrement," +
-                ROLE_NAME_COLUMN +        " text not null);";
-
-        String dutyTable = "create table " + DUTY_TABLE + "("+
-                DUTY_ID_COLUMN +                " integer primary key autoincrement," +
-                DUTY_FROM_COLUMN +              " text not null," +
-                DUTY_TO_COLUMN +                " text not null," +
-                DUTY_TYPE_FK_COLUMN +           " integer references "+TYPES_TABLE+" ("+TYPES_ID_COLUMN+")," +
-                DUTY_MAX_PEOPLE_COLUMN +        " integer not null);";
-
-        String personTable = "create table " + PERSON_TABLE + "("+
-                PERSON_ID_COLUMN +               " integer primary key autoincrement," +
-                PERSON_LOGIN_COLUMN +            " text not null," +
-                PERSON_FIO_COLUMN +              " text," +
-                PERSON_TEL_COLUMN +              " text," +
-                PERSON_ADDRESS_COLUMN +          " text," +
-                PERSON_BIRTH_COLUMN +            " text," +
-                PERSON_IMAGE_COLUMN +            " BLOB," +
-                PERSON_ROLE_COLUMN +             " integer references "+ROLE_TABLE+" ("+ROLE_ID_COLUMN+")"+");";
-
-        String personOnDuty = "create table " + PER_ON_DUTY_TABLE + "("+
-                PER_ON_DUTY_ID_COLUMN +          " integer primary key autoincrement," +
-                PER_ON_DUTY_PERSON_FK_COLUMN +   " integer references "+PERSON_TABLE+" ("+PERSON_ID_COLUMN+")," +
-                PER_ON_DUTY_DUTY_FK_COLUMN +     " integer references "+DUTY_TABLE+" ("+DUTY_ID_COLUMN+")," +
-                PER_ON_DUTY_FROM_COLUMN +        " text not null," +
-                PER_ON_DUTY_TO_COLUMN +          " text not null);";
-
-        db.execSQL(typeTable);
-        db.execSQL(roleTable);
-        db.execSQL(dutyTable);
-        db.execSQL(personOnDuty);
-        db.execSQL(personTable);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
-    }
 }

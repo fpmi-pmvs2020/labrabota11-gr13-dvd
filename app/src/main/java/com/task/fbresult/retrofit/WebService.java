d6c250f33a23350ec357e67c018c61446a11f7d1
dmitars <dmitriy.tarasienko@gmail.com>
2021-04-05 12:53:45 +0300

    refactor: optimize imports

--- app/src/main/java/com/task/fbresult/retrofit/WebService.java
+++ app/src/main/java/com/task/fbresult/retrofit/WebService.java
@@ -1,21 +1,14 @@
 package com.task.fbresult.retrofit;
 
-import com.task.fbresult.db.dao.PeopleOnDutyDao;
 import com.task.fbresult.model.GraphicResult;
 import com.task.fbresult.model.HourlyGraphicResult;
-import com.task.fbresult.model.PeopleOnDuty;
 import com.task.fbresult.model.PostModel;
-
-import java.util.List;
-import java.util.Observable;
 
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Body;
-import retrofit2.http.GET;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
 
 public interface WebService {
 

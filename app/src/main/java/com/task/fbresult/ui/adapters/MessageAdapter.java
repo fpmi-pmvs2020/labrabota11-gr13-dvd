9c04188428d3bcd8eede9c8bb4a1e81497874417
dmitars <dmitriy.tarasienko@gmail.com>
2021-04-11 16:41:01 +0300

    fix: list of messages interface

--- app/src/main/java/com/task/fbresult/ui/adapters/MessageAdapter.java
+++ app/src/main/java/com/task/fbresult/ui/adapters/MessageAdapter.java
@@ -15,10 +15,13 @@ import com.task.fbresult.R;
 import com.task.fbresult.db.fbdao.FBDutyDao;
 import com.task.fbresult.db.fbdao.FBPeopleOnDutyDao;
 import com.task.fbresult.db.fbdao.FBPersonDao;
+import com.task.fbresult.model.DutyIntervalData;
 import com.task.fbresult.model.MyMessage;
+import com.task.fbresult.model.PeopleOnDuty;
 import com.task.fbresult.model.Person;
 import com.task.fbresult.ui.holders.MessageViewHolder;
 import com.task.fbresult.util.FBUtils;
+import com.task.fbresult.util.MessageUtils;
 
 import java.util.List;
 
@@ -56,18 +59,24 @@ public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
         MyMessage myMessage = items.get(position);
-        FBPersonDao personDao = new FBPersonDao();
         MessageViewHolder holder = (MessageViewHolder) viewHolder;
-        FBPeopleOnDutyDao peopleOnDutyDao = new FBPeopleOnDutyDao();
+        Person sender = getOtherPersonForMessage(myMessage);
 
-        Person sender = personDao.getWithId(myMessage.getAuthorId());
         holder.senderFIO.setText(sender.getFio());
-        var peopleOnDuty = peopleOnDutyDao.getWithId(myMessage.getRecipientOnDutyId());
-        holder.toTime.setText(peopleOnDuty.getTo().split("T")[1]);
-        holder.fromTime.setText(peopleOnDuty.getFrom().split("T")[1]);
-        holder.date.setText(peopleOnDuty.getFrom().split("T")[0]);
-        switch (myMessage.getMessageState()){
+        DutyIntervalData messageTimeInfo;
+        if(myMessage.getRecipientIntervalData().getPeopleOnDutyId()!=null) {
+            messageTimeInfo = myMessage.getRecipientIntervalData();
+        }else{
+            messageTimeInfo = myMessage.getAuthorIntervalData();
+        }
 
+        holder.toTime.setText(messageTimeInfo.getTo().split("T")[1]);
+        holder.fromTime.setText(messageTimeInfo.getFrom().split("T")[1]);
+        holder.date.setText(messageTimeInfo.getFrom().split("T")[0]);
+
+        hideMarks(holder);
+
+        switch (myMessage.getMessageState()){
             case READ:
                 holder.checkMark.setVisibility(View.VISIBLE);
                 break;
@@ -78,6 +87,27 @@ public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
                 setRefused(holder);
         }
 
+    }
+
+    private PeopleOnDuty getPeopleOnDutyFromIntervalData(DutyIntervalData dutyIntervalData){
+        return new FBPeopleOnDutyDao().getWithId(dutyIntervalData.getPeopleOnDutyId());
+    }
+
+    private Person getOtherPersonForMessage(MyMessage myMessage){
+        if(MessageUtils.currentPersonIsAuthorOf(myMessage))
+            return getPersonFromDutyIntervalData(myMessage.getRecipientIntervalData());
+        else
+            return getPersonFromDutyIntervalData(myMessage.getAuthorIntervalData());
+    }
+
+    private Person getPersonFromDutyIntervalData(DutyIntervalData dutyIntervalData){
+        return new FBPersonDao().getWithId(dutyIntervalData.getPersonId());
+    }
+
+    private void hideMarks(MessageViewHolder holder){
+        holder.checkMark.setVisibility(View.INVISIBLE);
+        holder.checkMarkGreen.setVisibility(View.INVISIBLE);
+        holder.crossMark.setVisibility(View.INVISIBLE);
     }
 
     private void setAccepted(MessageViewHolder holder) {

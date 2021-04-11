a18aff54aed5e595427c28d242c39db3d066e8c7
dmitars <dmitriy.tarasienko@gmail.com>
2021-04-11 16:42:16 +0300

    feat: logic about intersected intervals

--- app/src/main/java/com/task/fbresult/util/DutyUtils.java
+++ app/src/main/java/com/task/fbresult/util/DutyUtils.java
@@ -4,7 +4,11 @@ import android.os.Build;
 
 import androidx.annotation.RequiresApi;
 
+import com.task.fbresult.db.fbdao.FBPeopleOnDutyDao;
+import com.task.fbresult.model.DutyIntervalData;
 import com.task.fbresult.model.PeopleOnDuty;
+
+import lombok.var;
 
 public class DutyUtils {
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -18,5 +22,31 @@ public class DutyUtils {
                 second.toAsLocalDateTime()
         );
         return firstInterval.intersects(secondInterval);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static boolean alreadyWorksOnThatTime(
+            String personId,
+            LocalDateTimeInterval goalDutyInterval,
+            PeopleOnDuty goalPeopleOnDuty) {
+        if(goalDutyInterval == null)
+            return false;
+        var goalDuty = DAORequester.getDutyWithPeopleOnDuty(goalPeopleOnDuty);
+        var goalPeopleOnDuties = DAORequester.getPeopleOnDuty(goalDuty);
+        for(var goalPeople:goalPeopleOnDuties){
+            if(goalPeople.getPersonId().equals(personId)){
+                if(containsInterval(goalPeople,goalDutyInterval))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private static boolean containsInterval(PeopleOnDuty peopleOnDuty, LocalDateTimeInterval interval){
+        var dutyFrom = LocalDateTimeHelper.parseString(peopleOnDuty.getFrom());
+        var dutyTo = LocalDateTimeHelper.parseString(peopleOnDuty.getTo());
+        var dutyLocalDateTimeInterval = new LocalDateTimeInterval(dutyFrom,dutyTo);
+        return dutyLocalDateTimeInterval.intersects(interval);
     }
 }

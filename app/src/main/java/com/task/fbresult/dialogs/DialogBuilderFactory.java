70e135d5c1d57b37bf86a14fa1b70d32c1bade75
dmitars <dmitriy.tarasienko@gmail.com>
2021-05-15 17:00:43 +0300

    fix: add new dialog's types to factory

--- app/src/main/java/com/task/fbresult/dialogs/DialogBuilderFactory.java
+++ app/src/main/java/com/task/fbresult/dialogs/DialogBuilderFactory.java
@@ -5,14 +5,20 @@ import android.content.Context;
 public class DialogBuilderFactory {
     private final Context context;
 
-    public DialogBuilderFactory(Context context){
+    public DialogBuilderFactory(Context context) {
         this.context = context;
     }
 
-    public DialogBuilder getDialogBuilder(DialogType type){
-        switch (type){
-            case PASSWORD_UPDATE: return new PasswordDialogBuilder(context);
-            default: return null;
+    public DialogBuilder getDialogBuilder(DialogType type) {
+        switch (type) {
+            case PASSWORD_UPDATE:
+                return new PasswordDialogBuilder(context);
+            case MAKE_EXCHANGE:
+                return new ExchangeDialogBuilder(context);
+            case EXCHANGE_ON_CURRENT:
+                return new ExchangeOnCurrentDialogBuilder(context);
+            default:
+                return null;
         }
     }
 }
